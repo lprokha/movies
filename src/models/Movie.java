868fc158371f3300a3c426c6dc2d02836c4d9b59
Leyla P. <liya1998@list.ru>
2025-12-15 02:32:38 +0600

    Added fields and inner classes to Movie.java

--- src/models/Movie.java
+++ src/models/Movie.java
@@ -1,5 +1,17 @@
 package models;
 
 public class Movie {
+    private String name;
+    private int year;
+    private String description;
+
+    private class Director {
+        private String fullName;
+    }
+
+    private class Cast {
+        private String fullName;
+        private String role;
+    }
 
 }
