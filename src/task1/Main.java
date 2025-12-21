269c8a3c3a95b2749ae09cdd1307456dd27401a6
Leyla P. <liya1998@list.ru>
2025-12-22 03:50:24 +0600

    Added package for task 1

---
+++ src/task1/Main.java
@@ -0,0 +1,16 @@
+package task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var cats = Cat.makeCats(10);
+        Printer.print(cats);
+
+        // А сюда добавьте код, который будет сортировать коллекцию котов
+        // используйте лямбда-выражения и ссылки на методы
+        // cats.sort(?);
+        // Printer.print(cats);
+    }
+
+}
