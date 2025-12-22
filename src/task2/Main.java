98216f87470f9a3eddd24b3fc7435921ed8eb112
Leyla P. <liya1998@list.ru>
2025-12-22 06:26:46 +0600

    Corrected pre-written constructors, added 3 more cats with unique actions

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -13,11 +13,13 @@ public class Main {
         
         // каждый кот должен уметь выполнять что-то своё
         var cats = new ArrayList<ActiveCat>();
-        cats.add(new ActiveCat(???));
-        cats.add(new ActiveCat(???));
-        cats.add(new ActiveCat(???));
+        cats.add(new ActiveCat(ActiveCat::eat));
+        cats.add(new ActiveCat(ActiveCat::jump));
+        cats.add(new ActiveCat(ActiveCat::sleep));
         // добавьте ещё два-три кота, с совершенно другими действиями
-
+        cats.add(new ActiveCat(() -> "Я охочусь на мышку!"));
+        cats.add(new ActiveCat(() -> "Я играю с клубком!"));
+        cats.add(new ActiveCat(() -> "Я вылизываю шерстку!"));
         cats.forEach(ActiveCat::doAction);
     }
 
